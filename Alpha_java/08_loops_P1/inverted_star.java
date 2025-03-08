626ba120b3e1d81f84d2ae32c25891889f49d45f
Adityahatake <shivanshdaksh567@gmail.com>
2025-03-08 23:16:28 +0530

    draft 08-march

--- Alpha_java/08_loops_P1/inverted_star.java
+++ Alpha_java/08_loops_P1/inverted_star.java
@@ -1,13 +1,13 @@
-public class star_Pattern {
+public class inverted_star {
 
-    // print this pattern ...
-    // *
-    // **
-    // ***
+    // print the following pattern ......
     // ****
+    // ***
+    // **
+    // *
     public static void main(String[] args) {
         for (int line = 1; line <= 4; line++) {
-            for (int star = 1; star <= line; star++) {
+            for (int star = 4; star >= line; star--) {
                 System.out.print("*");
             }
             System.out.println();
