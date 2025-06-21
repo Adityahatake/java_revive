757564609ed49e02df89ca77f985851964d8e3ba
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-21 23:28:03 +0530

    added strings

---
+++ Alpha_java/14_strings/StringIntro.java
@@ -0,0 +1,12 @@
+public class StringIntro {
+    public static void main(String[] args) {
+        // Creating string using double quotes (common way)
+        String name = "Aditya";
+
+        // Creating string using the new keyword
+        String college = new String("ABESIT");
+
+        System.out.println("Name: " + name);
+        System.out.println("College: " + college);
+    }
+}
