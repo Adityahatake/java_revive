2853c996ea140cabcb2f5c30ddb6dc86a3425f1c
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-22 23:10:37 +0530

    format

--- Alpha_java/14_strings/stringBuilderBuffer.java
+++ Alpha_java/14_strings/stringBuilderBuffer.java
@@ -1,3 +1,7 @@
+// Unlike String, both StringBuilder and StringBuffer are mutable — you can modify them without creating a new object every time.
+
+// Use them when you're doing many string operations (like appending in loops).
+
 public class stringBuilderBuffer {
     public static void main(String[] args) {
         //creating a string builder
@@ -8,7 +12,19 @@ public class stringBuilderBuffer {
         StringBuffer sbf=new StringBuffer("focus on yourself");
         sbf.insert(17, " bro ");
         System.out.println("String sbf: "+sbf);
+
+                  // Replace and delete
+        sb.replace(0, 5, "Hi");
+        sbf.delete(4, 15);
+        System.out.println("Modified StringBuilder: " + sb);
+        System.out.println("Modified StringBuffer: " + sbf);
+
         
     }
     
 }
+
+// Feature	   | StringBuilder	   |  StringBuffer
+// Thread-safe?| No	               |  Yes (synchronized)
+// Performance | Faster	           |  Slower (due to sync)
+// Use-case    |Single-thread apps | Multi-thread apps
