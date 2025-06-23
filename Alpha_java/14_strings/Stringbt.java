0fff983173c4650d7f9fcd90944b0b979f615733
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-23 23:20:29 +0530

    reloading string

---
+++ Alpha_java/14_strings/Stringbt.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Stringbt {
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        String str1 = "daksh";
+        String str2 = new String("daksh");
+
+        // Compare references (this will print false)
+        System.out.println(str1 == str2);
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input for programming language
+        System.out.print("Enter the name of your current programming language: ");
+        String lang = sc.nextLine();  // Use nextLine to read full input
+        System.out.println("Language: " + lang);
+
+        // Input for favorite hero
+        System.out.print("Enter the name of your fav hero: ");
+        String hero = sc.nextLine();  // Will now wait properly for input
+        System.out.println("Hero: " + hero);
+
+        sc.close();
+    }
+}
