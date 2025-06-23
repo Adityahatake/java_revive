b24f2b8c5373f4f3a6c36cae8ea4cc4353c077ab
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-23 23:54:15 +0530

    shortest path

---
+++ Alpha_java/14_strings/routeDistance.java
@@ -0,0 +1,32 @@
+public class routeDistance {
+    public static void getShortestDistance(String path) {
+        int x=0 ,y=0;
+
+        for(int i=0;i<path.length();i++){
+           //west
+           if(path.charAt(i)=='S'){
+            x--;
+           } 
+           //east
+            if(path.charAt(i)=='S'){
+            x++;
+           } 
+           //south 
+           if(path.charAt(i)=='S'){
+            y--;
+           } 
+           //north
+           if(path.charAt(i)=='S'){
+            y++;
+           }        
+    }
+    int x2=x*x;
+    int y2=y*y;
+    int finaldistance=(int)Math.sqrt(x2+y2);
+    System.out.println("Final Shortest Distance is: "+finaldistance);
+}
+    public static void main(String[] args) {
+        String path="WNEENESENNN";
+        getShortestDistance(path);
+    }
+}
