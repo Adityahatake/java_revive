a6ae99c0667d9cc8327fb83ba76ca892fa442516
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-25 23:31:20 +0530

    added String compression(amazon)

--- Alpha_java/14_strings/stringCompression.java
+++ Alpha_java/14_strings/stringCompression.java
@@ -1,5 +1,25 @@
 public class stringCompression {
+    public static void string_compression(String str) {
+        StringBuilder newstr=new StringBuilder("");
+
+        for(int i=0;i<str.length();i++){
+        Integer count=0;
+        while (i<str.length()-1 && str.charAt(i)==str.charAt(i+1)) {
+            count++;
+            i++;
+        }
+        newstr=newstr.append(str.charAt(i));
+        if(count>1){
+            newstr=newstr.append(count.toString());
+        }
+       
+    }
+     newstr.toString();
+        System.out.println(newstr);
+}
     public static void main(String[] args) {
         System.out.println("Test");
+        String str="aaaabccc";
+        string_compression(str);
     }
 }
