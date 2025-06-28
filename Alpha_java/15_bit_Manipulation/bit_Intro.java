7c062cf2d9e27fcb83e5a4414058b1f33aaa03c4
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-28 23:04:50 +0530

    commit

---
+++ Alpha_java/15_bit_Manipulation/bit_Intro.java
@@ -0,0 +1,8 @@
+public class bit_Intro {
+
+    public static void main(String[] args) {
+        System.out.println(5&6); //binary and(&)
+        System.out.println(5|6); //binary or(|)
+        System.out.println(5^6); //binary xor(^)
+    }
+}
