a16e547162fe33cff872f8ecbd8e8a62970486c0
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-28 23:31:37 +0530

    complete intro

--- Alpha_java/15_bit_Manipulation/bit_Intro.java
+++ Alpha_java/15_bit_Manipulation/bit_Intro.java
@@ -5,5 +5,8 @@ public class bit_Intro {
         System.out.println(5|6); //binary or(|)
         System.out.println(5^6); //binary xor(^)
         System.out.println(~5); //one's complement(~)
+        System.out.println(5<<2); //binary left shift(<<) , (a<<b =a*2^b)
+        System.out.println(6>>1); //binary right shift(>>) , (a<<b =a\2^b)
+
     }
 }
