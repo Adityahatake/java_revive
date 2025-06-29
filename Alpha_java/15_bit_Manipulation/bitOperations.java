114cb52ff6de91d6bb5db92ba8772787df863085
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-29 23:13:06 +0530

    update

--- Alpha_java/15_bit_Manipulation/bitOperations.java
+++ Alpha_java/15_bit_Manipulation/bitOperations.java
@@ -8,9 +8,16 @@ public class bitOperations {
         }
 
     }
+    public static void set_iTH_bit(int n ,int i) {
+        int bitmask =1<<i;
+        System.out.println(n | bitmask);
+    }
+
+
     public static void main(String[] args) {
         System.out.println("test   ");
         get_iTH_bit(10, 3);
+        set_iTH_bit(10, 2);
     }
     
 }
