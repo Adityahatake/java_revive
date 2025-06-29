273ce69b711a30051921b3cd97bba07ad1a97f44
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-29 23:28:34 +0530

    update draft

--- Alpha_java/15_bit_Manipulation/bitOperations.java
+++ Alpha_java/15_bit_Manipulation/bitOperations.java
@@ -1,21 +1,31 @@
 public class bitOperations {
-    public static void get_iTH_bit(int n ,int i) {
-        int bitmask=1<<i;
-        if((n & bitmask)==0){
-            System.out.println("bit is: "+0);
-        }else{
-            System.out.println("bit is: "+1);
+    public static void get_iTH_bit(int n, int i) {
+        int bitmask = 1 << i;
+        if ((n & bitmask) == 0) {
+            System.out.println("bit is: " + 0);
+        } else {
+            System.out.println("bit is: " + 1);
         }
 
     }
-    public static void set_iTH_bit(int n ,int i) {
-        int bitmask =1<<i;
+
+    public static void set_iTH_bit(int n, int i) {
+        int bitmask = 1 << i;
         System.out.println(n | bitmask);
     }
-     public static void clear_iTH_bit(int n ,int i){
-        int bitmask= ~(1<<i);
-        System.out.println(n&bitmask);
-     }
+
+    public static void clear_iTH_bit(int n, int i) {
+        int bitmask = ~(1 << i);
+        System.out.println(n & bitmask);
+    }
+    public static void update_iTH_bit(int n, int i ,int newBit) {
+        if(newBit==0){
+            clear_iTH_bit(n, i);
+        }
+        if(newBit==1){
+            set_iTH_bit(n, i);
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -23,6 +33,7 @@ public class bitOperations {
         get_iTH_bit(10, 3);
         set_iTH_bit(10, 2);
         clear_iTH_bit(10, 1);
+        update_iTH_bit(10, 2, 1);
     }
-    
+
 }
