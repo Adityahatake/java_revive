72c4e1e66490811de29c76345399a76be40eddf7
Adityahatake <shivanshdaksh567@gmail.com>
2025-06-30 22:44:52 +0530

    draft 30 june

--- Alpha_java/15_bit_Manipulation/bitOperations.java
+++ Alpha_java/15_bit_Manipulation/bitOperations.java
@@ -18,15 +18,27 @@ public class bitOperations {
         int bitmask = ~(1 << i);
         System.out.println(n & bitmask);
     }
-    public static void update_iTH_bit(int n, int i ,int newBit) {
-        if(newBit==0){
+
+    public static void update_iTH_bit(int n, int i, int newBit) {
+        if (newBit == 0) {
             clear_iTH_bit(n, i);
         }
-        if(newBit==1){
+        if (newBit == 1) {
             set_iTH_bit(n, i);
         }
     }
 
+    public static void clear_last_iTH_bit(int n, int i) {
+        int bitmask = ~(0) << i;
+        System.out.println(n & bitmask);
+    }
+
+    public static void clear_Range_of_iTH_bit(int n, int i, int j) {
+        int a = ((~0) << (j + 1));
+        int b = (1 << i) - 1;
+        int bitmask = a | b;
+        System.out.println(n & bitmask);
+    }
 
     public static void main(String[] args) {
         System.out.println("test");
@@ -34,6 +46,8 @@ public class bitOperations {
         set_iTH_bit(10, 2);
         clear_iTH_bit(10, 1);
         update_iTH_bit(10, 2, 1);
+        clear_last_iTH_bit(15, 2);
+        clear_Range_of_iTH_bit(10, 2, 4);
     }
 
 }
