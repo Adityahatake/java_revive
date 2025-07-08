3d275b3673bc8f1d999c149f8a0b6f5f6c39f820
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-08 22:50:21 +0530

    draft 8 july

---
+++ Java_Problems/palindrome_LT9.java
@@ -0,0 +1,23 @@
+public class palindrome_LT9 {
+
+    public static boolean ispalindrome(int n) {
+        int originalnum = n;
+        int reversenum = 0;
+
+        while (n != 0) {
+            int lastdigit = n % 10; // getting the last digit
+            reversenum = reversenum * 10 + lastdigit; // building the reverse for comparasion
+            n = n / 10; // removing the last digit
+        }
+        if(reversenum==originalnum){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(ispalindrome(121));
+        System.out.println(ispalindrome(12321));
+        System.out.println(ispalindrome(1232));
+    }
+}
