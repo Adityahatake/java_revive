82dbddec041218aa57ae23e994333c7be6aa8da7
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-14 23:28:02 +0530

    u

---
+++ Alpha_java/16_Oops_P1/Oops_Intro.java
@@ -0,0 +1,16 @@
+public class Oops_Intro {
+    public static void main(String[] args) git{
+        
+    }
+}
+class Pen{
+    String color;
+    int tipSize;
+
+    void setColor(String newcolor){
+        color=newcolor;
+    }
+    void setTip(int newTip){
+        tipSize=newTip;
+    }
+}
