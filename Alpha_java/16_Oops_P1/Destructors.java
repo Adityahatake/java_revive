f1da01d49cdbd8beee063744530cf6fdc4db11c6
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-15 23:30:52 +0530

    u

---
+++ Alpha_java/16_Oops_P1/Destructors.java
@@ -0,0 +1,6 @@
+public class Destructors {
+    public static void main(String[] args) {
+        System.out.println("Lol!");
+        //java has Garbage collector so it doesn't require destrctors
+    }
+}
