d4ea680184c849806db7d807b432da2fbb56892d
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-15 23:08:13 +0530

    final draft 14july

---
+++ Alpha_java/16_Oops_P1/Constructors.java
@@ -0,0 +1,27 @@
+public class Constructors {
+    public static void main(String[] args) {
+        System.out.println("test");
+        Student s1=new Student();
+        s1.name="Anjali";
+        System.out.println(s1.name);
+        Student s2=new Student("aditya" , 10);
+        Student s3= new Student(s2);
+        System.out.println(s3.roll);
+    }
+}
+class Student{
+    String name;
+    int roll;
+    int  marks[] =new int[3];
+    Student(){ //non parameterized constructor
+        System.out.println(" non parameterized constrctor is called");
+    }
+    Student(String name,int roll){ //parameterized constructor
+        this.name=name;
+        this.roll=roll;
+    }
+    Student(Student s){ //copy constructor
+        this.name=s.name;
+        this.roll=s.roll;
+    }
+}
