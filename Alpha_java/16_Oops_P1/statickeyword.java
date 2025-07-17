11c8d0a721fb0c376bc07a40813f02ca238eb927
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-17 23:24:17 +0530

    add static (17 july)

---
+++ Alpha_java/16_Oops_P1/statickeyword.java
@@ -0,0 +1,30 @@
+public class statickeyword {
+      public static void main(String[] args) {
+        StaticExample obj1 = new StaticExample();
+        StaticExample obj2 = new StaticExample();
+
+        obj1.incrementCount();
+        obj2.incrementCount();
+
+        // Accessing static method without object
+        StaticExample.displayCount(); // Output: Count is: 2
+        //here single count variable is shraed among all the object 
+        
+    }
+}
+class StaticExample {
+    static int count = 0; // static variable shared across all objects
+
+    // static method
+    static void displayCount() {
+        System.out.println("Count is: " + count);
+    }
+
+    // non-static method
+    void incrementCount() {
+        count++;
+    }
+
+  
+}
+
