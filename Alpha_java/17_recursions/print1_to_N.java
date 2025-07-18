e85b56148d83f7ff53f806eff8b5fa23b3d8031b
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-18 23:32:27 +0530

    u

---
+++ Alpha_java/17_recursions/print1_to_N.java
@@ -0,0 +1,13 @@
+public class print1_to_N {
+    public static void main(String[] args) {
+        printOnetoN(5);
+    }
+    public static void printOnetoN(int n) {
+        if (n == 1) {
+            System.out.print(" " + 1);
+        } else {
+            printOnetoN(n - 1);
+            System.out.print(" " + n);
+        }
+    }
+}
