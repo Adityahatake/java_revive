e5769b3d966d300446e2b210068780e194af99d4
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-19 23:58:40 +0530

    u

---
+++ Alpha_java/17_recursions/sortedcheck.java
@@ -0,0 +1,13 @@
+public class sortedcheck {
+        public static void main(String[] args) {
+             System.out.println("");    
+        }  
+
+
+        public static boolean ifSorted(int arr[]) {
+            if(arr[0]>arr[1]){
+                return true;
+            }
+            return false;
+        }
+}
