e455a6d8ffde948172f5632fcf710e30a540f73d
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-19 23:54:30 +0530

    u

--- Alpha_java/17_recursions/nth_Fibonaccci.java
+++ Alpha_java/17_recursions/nth_Fibonaccci.java
@@ -1,6 +1,12 @@
 public class nth_Fibonaccci {
     public static void main(String[] args) {
-        System.out.println(fibonacccinum(50));
+        System.out.println(fibonacccinum(5));
+        //to print it 
+        int count =10;
+        for(int i=0; i<count; i++){
+            System.out.print(fibonacccinum(i) + " ");
+        }
+
     }
     public static int fibonacccinum(int n) {
         if(n==0){
