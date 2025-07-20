5b29169cdd1700b65903f3010e3e42cef4d88264
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-20 23:23:50 +0530

    draft 20july

--- Alpha_java/21_Linked_list/sample.java
+++ Alpha_java/21_Linked_list/sample.java
@@ -1,5 +1,6 @@
 public class sample{
     public static void main(String[] args) {
-        System.out.println("sam ");
+        System.out.println("sam");
+        System.out.println("vero");
     }
 }
