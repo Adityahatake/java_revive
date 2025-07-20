ff4e3dac33d93a2009de87b64dbf3b38658a63e3
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-20 23:26:16 +0530

    u

--- Alpha_java/21_Linked_list/sample.java
+++ Alpha_java/21_Linked_list/sample.java
@@ -2,5 +2,6 @@ public class sample{
     public static void main(String[] args) {
         System.out.println("sam");
         System.out.println("vero");
+        System.out.println("2d array is left");
     }
 }
