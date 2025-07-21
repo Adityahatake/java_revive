8ee4d63ec804083aac8c09c69a04801af30d5878
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-21 23:14:30 +0530

    u draft 21 july

--- Alpha_java/17_recursions/sortedcheck.java
+++ Alpha_java/17_recursions/sortedcheck.java
@@ -1,13 +1,19 @@
 public class sortedcheck {
-        public static void main(String[] args) {
-             System.out.println("");    
-        }  
-
-
-        public static boolean ifSorted(int arr[]) {
-            if(arr[0]>arr[1]){
-                return true;
-            }
+    public static void main(String[] args) {
+        int arr1[]={2,4,5,6,7};
+        int arr2[]={2,6,5,8,7};
+        System.out.println(ifsorted(arr1, 0));
+        System.out.println(ifsorted(arr2, 0));
+        
+    }
+    public static boolean ifsorted(int[] arr, int i) {
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
             return false;
         }
+        return ifsorted(arr, i+1);
+    }
+
 }
