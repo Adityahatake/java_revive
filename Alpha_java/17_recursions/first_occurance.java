d0adf7f3832b991414e64f1c89b0d1156dabb1f6
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-21 23:31:57 +0530

    u

---
+++ Alpha_java/17_recursions/first_occurance.java
@@ -0,0 +1,20 @@
+public class first_occurance {
+    public static void main(String[] args) {
+        int arr[]={5,6,7,};
+       foccurance(arr, 5, 0);
+        
+    }
+    public static void foccurance(int[] arr ,int key , int i) {
+        if(i==arr.length-1){
+            System.out.println("not found bro");
+        }if(arr[i]==key){
+            System.out.println("found at index: "+i);
+        }else{
+        foccurance(arr, key ,i+1);
+        }
+    }
+}
+// need to write a code using recursion 
+// to find the first iccurance of a element in
+// an array
+
