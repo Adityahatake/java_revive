15ef3b2f8a6c1c3cc11946b4d94fe7adeadadd12
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-22 23:15:55 +0530

    draft 22nd july u

--- Alpha_java/17_recursions/power_opt.java
+++ Alpha_java/17_recursions/power_opt.java
@@ -1,17 +1,19 @@
 public class power_opt{
     // complexity : O(logn)
     public static void main(String[] args) {
-        System.out.println(opt_power(2, 4));
+        System.out.println(opt_power(2, 5));
         
     }
     public static int opt_power(int x , int n) {
         if(n==0){
             return 1;
         }
-        if(n%2==0){
-            return opt_power(x, n/2)*opt_power(x, n/2);
-        }else{
-            return x*opt_power(x, n/2)*opt_power(x, n/2);
+        int halfpower=opt_power(x, n/2);                
+        
+        if(n%2==0){ //if n is even
+            return halfpower*halfpower;
+        }else{//if n is odd
+            return x*halfpower*halfpower;
         }
     }
 }
