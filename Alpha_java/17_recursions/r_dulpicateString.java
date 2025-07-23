9ff628fff31ccc2ee8a9228db8731ba5fe581cec
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-23 23:41:30 +0530

    u

--- Alpha_java/17_recursions/r_dulpicateString.java
+++ Alpha_java/17_recursions/r_dulpicateString.java
@@ -1,5 +1,23 @@
 public class r_dulpicateString {
     public static void main(String[] args) {
+       String name ="aadityaaadaksh";
+       removeDuplicates(name, 0, new StringBuilder(), new boolean[26]);
+        
+    }
+    public static void removeDuplicates(String str , int idx , StringBuilder newstr, boolean map[]) {
+        if(idx==str.length()){ //base case
+            System.out.println(newstr);
+            return;
+        }
+        //kaam
+        char currChar=str.charAt(idx);
+        if(map[currChar-'a']==true){
+            //duplicate
+            removeDuplicates(str, idx+1, newstr, map);
+        }else{
+            map[currChar-'a']=true;
+            removeDuplicates(str, idx+1, newstr.append(currChar), map);
+        }
         
     }
 }
