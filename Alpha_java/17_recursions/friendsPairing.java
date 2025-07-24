598586287d25a25842188dc6ee2cbed080b3f5f8
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-24 23:24:27 +0530

    u

--- Alpha_java/17_recursions/friendsPairing.java
+++ Alpha_java/17_recursions/friendsPairing.java
@@ -1,9 +1,11 @@
 public class friendsPairing {
     public static void main(String[] args) {
-        
+
     }
 }
-/*Given n friends, each one can remain single or can be paired 
-up with some other friend. Each friend can be paired only once.
- Find out the total number of ways in which friends can remain 
- single or can be paired up.  */ 
+/*
+ * Given n friends, each one can remain single or can be paired
+ * up with some other friend. Each friend can be paired only once.
+ * Find out the total number of ways in which friends can remain
+ * single or can be paired up.
+ */
