5ce9e4f0a11686eb6f19ec642efb77de458d2f27
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-28 23:58:46 +0530

    added func

--- Alpha_java/18_DivideN_Conquer/Merge_sort.java
+++ Alpha_java/18_DivideN_Conquer/Merge_sort.java
@@ -1,9 +1,18 @@
 public class Merge_sort{
     public static void main(String[] args){
         int[] a1={6,3,9,5,2,8};
+        printArray(a1);
 
     }
     public static void mergeSort(int[] arr) {
-        int mid= ;
+        //base case
+        // int mid= ;
+        // kaam 
+
+    }
+    public static void printArray(int[] arr) {
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i] +" ");
+        }
     }
 }
