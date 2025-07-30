0bada6bb16a833e2dabab19e72f7badd45135dcf
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-30 23:57:15 +0530

    u final draft

--- Alpha_java/18_DivideN_Conquer/Quick_sort.java
+++ Alpha_java/18_DivideN_Conquer/Quick_sort.java
@@ -1,5 +1,21 @@
 public class Quick_sort {
     public static void main(String[] args) {
-        int arr[]={6,3,9,8,2,5};
+        int arr[] = { 6, 3, 9, 8, 2, 5 };
+        quickSort(arr, 0, arr.length-1);
+
     }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void quickSort(int[] arr , int si , int ei) {
+        
+        //last element
+      int pidx=  partition(arr , si , ei); //pivot index
+    }
+
 }
