75ba3bfeec4ae757a2f1faef3b918c845b7600f8
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-30 23:16:30 +0530

    u

--- Alpha_java/18_DivideN_Conquer/Merge_sort.java
+++ Alpha_java/18_DivideN_Conquer/Merge_sort.java
@@ -1,6 +1,6 @@
-public class Merge_sort{
-    public static void main(String[] args){
-        int[] a1={6,3,9,5,2,8};
+public class Merge_sort {
+    public static void main(String[] args) {
+        int[] a1 = { 6, 3, 9, 5, 2, 8 };
         System.out.println("unsorted array:");
         printArray(a1);
         System.out.println("sorted array: ");
@@ -8,54 +8,56 @@ public class Merge_sort{
         printArray(a1);
 
     }
-    public static void mergeSort(int[] arr, int si , int ei) {
-        //base case
-        if(si>=ei){
+
+    public static void mergeSort(int[] arr, int si, int ei) {
+        // base case
+        if (si >= ei) {
             return;
         }
-        // kaam 
-        int mid= si+(ei-si)/2; //(si+ei)/2
-        mergeSort(arr, si, mid); //left part
-        mergeSort(arr, mid+1, ei); //right part
+        // kaam
+        int mid = si + (ei - si) / 2; // (si+ei)/2
+        mergeSort(arr, si, mid); // left part
+        mergeSort(arr, mid + 1, ei); // right part
         merge(arr, si, mid, ei);
 
     }
-    public static void merge(int[] arr, int si ,int mid , int ei) {
-        int temp[]=new int[ei-si+1];
-        int i =si ;//iterator for left part
-        int j= mid+1; //iterator for right part
-        int k =0; // iterator for temp array 
-        while (i<=mid && j<=ei) {
-            if(arr[i]<arr[j]){
-                temp[k]=arr[i];
-                i++;            
-            }else{
-                temp[k]=arr[j];
+
+    public static void merge(int[] arr, int si, int mid, int ei) {
+        int temp[] = new int[ei - si + 1];
+        int i = si;// iterator for left part
+        int j = mid + 1; // iterator for right part
+        int k = 0; // iterator for temp array
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp[k] = arr[i];
+                i++;
+            } else {
+                temp[k] = arr[j];
                 j++;
             }
             k++;
         }
         // if left part is left
-        while (i<=mid) {
-            temp[k++]=arr[i++];
+        while (i <= mid) {
+            temp[k++] = arr[i++];
 
         }
         // if right part is left
 
-        while (j<=ei) {
-            temp[k++]=arr[j++];
+        while (j <= ei) {
+            temp[k++] = arr[j++];
 
         }
 
-        //copy temp to og array
-        for(k=0 , i=si; k<temp.length; k++ , i++){
-            arr[i]=temp[k];
+        // copy temp to og array
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
         }
     }
 
     public static void printArray(int[] arr) {
-        for(int i =0;i<arr.length;i++){
-            System.out.print(arr[i] +" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
