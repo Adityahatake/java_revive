0e06621dc07721ad2c1acfb45e31a8bfbcdeb1ba
Adityahatake <shivanshdaksh567@gmail.com>
2025-07-31 23:27:13 +0530

    u draft 31 july

--- Alpha_java/18_DivideN_Conquer/Quick_sort.java
+++ Alpha_java/18_DivideN_Conquer/Quick_sort.java
@@ -2,7 +2,8 @@ public class Quick_sort {
     public static void main(String[] args) {
         int arr[] = { 6, 3, 9, 8, 2, 5 };
         quickSort(arr, 0, arr.length-1);
-        
+        printArray(arr);
+
 
     }
 
@@ -14,9 +15,36 @@ public class Quick_sort {
     }
 
     public static void quickSort(int[] arr , int si , int ei) {
-        
+        //base case
+        if (si >= ei) {
+            return;
+        }
         //last element
-      int pidx=  partition(arr , si , ei); //pivot index
+        int pidx = partition(arr, si, ei); //pivot index
+        quickSort(arr, si, pidx-1); //left
+        quickSort(arr, pidx+1, ei); //right
+    }
+    public static int partition(int[] arr , int si , int ei) {
+        int pivot =arr[ei];
+        int i =si-1 ; //to make place for els smaller than pivot
+
+        for(int j=si; j<ei;j++){
+            if(arr[j]<=pivot){
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+            }
+        }
+        // pivot ko sahi place pe lana
+        i++;
+        int temp = pivot;
+                arr[ei]=arr[i]; //pivot =arr[i](wrong)
+                arr[i]=temp;
+                return i;
+
+        
     }
 
 }
