cf3a0c6c5d227aa215fa5d04c758316e8160413c
Adityahatake <shivanshdaksh567@gmail.com>
2025-08-02 23:29:53 +0530

    described problem

---
+++ Alpha_java/13_2D_Arrays/problem1.java
@@ -0,0 +1,17 @@
+//example input : 
+// int[][] matrix = {
+//     {1, 2, 3},
+//     {4, 5, 6},
+//     {7, 8, 9}
+// };
+//expected output :
+// Sum of row 0: 6
+// Sum of row 1: 15
+// Sum of row 2: 24
+
+
+public class problem1 {
+    
+}
+
+
