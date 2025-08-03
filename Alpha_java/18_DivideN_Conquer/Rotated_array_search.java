54acaae6f4d90d6159eceea5abe048d7ff7a5b0e
Adityahatake <shivanshdaksh567@gmail.com>
2025-08-03 23:12:28 +0530

    final 3rd july

--- Alpha_java/18_DivideN_Conquer/Rotated_array_search.java
+++ Alpha_java/18_DivideN_Conquer/Rotated_array_search.java
@@ -1,6 +1,14 @@
 public class Rotated_array_search {
     public static void main(String[] args) {
         System.out.println("test");
-        int[] a1={4,5,6,0,1,2}; //rotated array (given)
+        int[] a1={4,5,6,0,1,2}; //rotated array (given )
+        printArray(a1);
     }
+       public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    
+}
 }
