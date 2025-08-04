fa0baa6f1a0cdeee15b389b15defba834f860df0
Adityahatake <shivanshdaksh567@gmail.com>
2025-08-04 23:53:30 +0530

    final draft 4 august

--- Alpha_java/18_DivideN_Conquer/Rotated_array_search.java
+++ Alpha_java/18_DivideN_Conquer/Rotated_array_search.java
@@ -1,14 +1,39 @@
 public class Rotated_array_search {
     public static void main(String[] args) {
-        System.out.println("test");
-        int[] a1={4,5,6,0,1,2}; //rotated array (given )
-        printArray(a1);
+       System.out.println("test");
+      int[] a1={4,5,6,0,1,2}; //rotated array (given )
+       printArray(a1);
     }
-       public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
+    public static void printArray(int[] arr) {
+      for (int i = 0; i < arr.length; i++) {
+          System.out.print(arr[i] + " ");
+      }
+      System.out.println();
     
+}
+// Function to search for a target in a rotated sorted array
+public static int searchRotatedArray(int[] arr, int target) {
+    int left = 0, right = arr.length - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        // Check if left half is sorted
+        if (arr[left] <= arr[mid]) {
+            if (target >= arr[left] && target < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else { // Right half is sorted
+            if (target > arr[mid] && target <= arr[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1; // Not found
 }
 }
