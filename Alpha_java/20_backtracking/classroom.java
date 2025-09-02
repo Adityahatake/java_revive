3d47f13f11f5bd221b36a6ce578f66fd8cf39030
Adityahatake <shivanshdaksh567@gmail.com>
2025-09-02 23:36:12 +0530

    2 sep

--- Alpha_java/20_backtracking/classroom.java
+++ Alpha_java/20_backtracking/classroom.java
@@ -1,17 +1,28 @@
-public class classroom{
+public class classroom {
     public static void main(String[] args) {
         System.out.println("test");
-        int arr[]=new int[5];
-        int num[]={1,2,3,4,5};
+        int arr[] = new int[5];
+        int num[] = { 1, 2, 3, 4, 5 };
         printarray(num);
-}
-public static void changearr(int[] arr, int i, int val) {
-    
-}
-public static void printarray(int[] arr) {
-    for(int i=0;i<arr.length;i++){
-        System.out.print(arr[i]+" ");
     }
-    // System.out.println();
-}
+
+    public static void changearr(int[] arr, int i, int val) {
+        // base case
+        if (i == arr.length) {
+            printarray(arr);
+            return;
+
+        }
+        // kaam
+        arr[i] = val;
+        changearr(arr, i + 1, val + 1);
+        arr[i] = arr[i] - 2;
+    }
+
+    public static void printarray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        // System.out.println();
+    }
 }
