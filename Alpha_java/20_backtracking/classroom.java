85d3c9c431f3377e5566c73efc0fb79e46e3fd72
Adityahatake <shivanshdaksh567@gmail.com>
2025-09-02 23:31:41 +0530

    2 sep

---
+++ Alpha_java/20_backtracking/classroom.java
@@ -0,0 +1,17 @@
+public class classroom{
+    public static void main(String[] args) {
+        System.out.println("test");
+        int arr[]=new int[5];
+        int num[]={1,2,3,4,5};
+        printarray(num);
+}
+public static void changearr(int[] arr, int i, int val) {
+    
+}
+public static void printarray(int[] arr) {
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+    }
+    // System.out.println();
+}
+}
