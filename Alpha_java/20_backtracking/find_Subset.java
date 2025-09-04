dd3bc104e0b2f930b6e3b41c0903cc0defe2dd58
Adityahatake <shivanshdaksh567@gmail.com>
2025-09-04 23:30:45 +0530

    4 sep

---
+++ Alpha_java/20_backtracking/find_Subset.java
@@ -0,0 +1,21 @@
+public class find_Subset {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        String str = "abc";
+        findsubset(str, "", 0);
+
+    }
+
+    public static void findsubset(String str, String ans, int i) {
+        // base case
+        if (i == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+        // yes choice
+        findsubset(str, ans + str.charAt(i), i + 1);
+        // no choice
+        findsubset(str, ans, i + 1);
+
+    }
+}
