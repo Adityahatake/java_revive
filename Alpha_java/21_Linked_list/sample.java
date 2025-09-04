b7639e8e112717b4b26e817638ac6c71a959d4d1
Adityahatake <shivanshdaksh567@gmail.com>
2025-09-04 10:53:30 +0530

    u

--- Alpha_java/21_Linked_list/sample.java
+++ Alpha_java/21_Linked_list/sample.java
@@ -1,7 +1,7 @@
 public class sample{
     public static void main(String[] args) {
         System.out.println("sam");
-        System.out.println("vero");
+        System.out.println("ver   rrro");
         System.out.println("2d array is left");
         System.out.println("will complete recursion tomaarow");
     }
