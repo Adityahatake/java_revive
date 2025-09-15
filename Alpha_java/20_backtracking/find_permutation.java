40d7cc2634625aee665435b8bf9cd359876ffd89
Adityahatake <shivanshdaksh567@gmail.com>
2025-09-15 23:35:30 +0530

    15  sep

--- Alpha_java/20_backtracking/find_permutation.java
+++ Alpha_java/20_backtracking/find_permutation.java
@@ -1,21 +1,24 @@
 public class find_permutation {
     public static void main(String[] args) {
-        System.out.println("test");
-        String str="test";
+        // System.out.println("test");
+        String str = "test";
+        permutation(str, " ");
     }
-    public static void permutation(String str , String ans) {
-        // base case
 
+    public static void permutation(String str, String ans) {
+        // base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
         // kaam (recursion)
-        for(int i=0;i<str.length();i++){
-            char curr=str.charAt(i);
-            //"abcde" = "ab" +"de" ="abde"
-            str=str.substring(0, i);str.substring(i+1);
-            permutation(str, ans+curr);
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            // "abcde" = "ab" +"de" ="abde"
+            String Newstr = str.substring(0, i);
+            str.substring(i + 1);
+            permutation(Newstr, ans + curr);
         }
 
-
-     
-       
     }
 }
