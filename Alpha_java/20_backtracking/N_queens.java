8ed95681853a4ee3af85e2681fd61b2869112d1d
Adityahatake <shivanshdaksh567@gmail.com>
2025-09-17 00:03:31 +0530

    16  sep

--- Alpha_java/20_backtracking/N_queens.java
+++ Alpha_java/20_backtracking/N_queens.java
@@ -1,9 +1,13 @@
 public class N_queens {
     public static void main(String[] args) {
-        
+        Matrix(4);
     }
     public static void Matrix(int n) {
-        if( int i = 0;i<n;i++){
+        for( int i = 0;i<n;i++){
+            for(int j=0; j<n;j++){
+                System.out.print("[]");
+            }
+            System.out.println();
             
         }
         
