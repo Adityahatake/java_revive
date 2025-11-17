359ebebafd277512e52c8094b39779fc44bfab84
Adityahatake <shivanshdaksh567@gmail.com>
2025-11-17 23:52:01 +0530

    U

--- Alpha_java/22_STACK/stack1.java
+++ Alpha_java/22_STACK/stack1.java
@@ -1,5 +1,12 @@
+import java.util.*;
 public class stack1 {
     public static void main(String[] args) {
-        
+         Stack<Integer> s = new Stack<>();
+         s.push(1);
+         s.push(2);
+         s.push(3);
+         s.push(4);
+         s.push(5);
+         System.out.println(s);
     }
 }
