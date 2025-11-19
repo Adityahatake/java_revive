966b9021afcd00492f3b6085243ff901aaeb7e2a
Adityahatake <shivanshdaksh567@gmail.com>
2025-11-19 23:51:10 +0530

    identifier Erorr persists

--- Alpha_java/22_STACK/stack1.java
+++ Alpha_java/22_STACK/stack1.java
@@ -4,7 +4,31 @@ public class stack1 {
         
     }
     public static void main(String[] args) {
-         Stack<Integer> s = new Stack<>();
+        public class stack1 {
+            public static void pushToBottom(Stack<Integer> s) {
+                if (s == null || s.isEmpty()) return;
+                int top = s.pop();
+                pushAtBottom(s, top);
+            }
+
+            private static void pushAtBottom(Stack<Integer> s, int value) {
+                if (s.isEmpty()) {
+                    s.push(value);
+                    return;
+                }
+                int top = s.pop();
+                pushAtBottom(s, value);
+                s.push(top);
+            }
+
+            public static void main(String[] args) {
+                 Stack<Integer> s = new Stack<>();
+                 s.push(1);
+                 s.push(2);
+                 s.push(3);
+                 System.out.println(s);
+            }
+        }
          s.push(1);
          s.push(2);
          s.push(3);
---
+++ Alpha_java/22_STACK/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+import java.util.*;
+public class stack1 {
+    public static void pushToBottom(Stack<Integer> s) {
+        
