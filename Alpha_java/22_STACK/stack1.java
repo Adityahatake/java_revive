cb833a0a72c361f086e964abae11e8a7b1810867
Adityahatake <shivanshdaksh567@gmail.com>
2025-11-19 23:47:54 +0530

    u

--- Alpha_java/22_STACK/stack1.java
+++ Alpha_java/22_STACK/stack1.java
@@ -8,9 +8,6 @@ public class stack1 {
          s.push(1);
          s.push(2);
          s.push(3);
-         s.push(4);
-         s.push(5);
-         s.push(6);
          System.out.println(s);
     }
 }
