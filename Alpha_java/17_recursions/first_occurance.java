84d800deae42f6a1ef2a4f59d23a0aa3ff1866ba
Adityahatake <shivanshdaksh567@gmail.com>
2025-12-10 23:16:11 +0530

    I

--- Alpha_java/17_recursions/first_occurance.java
+++ Alpha_java/17_recursions/first_occurance.java
@@ -2,7 +2,6 @@ public class first_occurance {
     public static void main(String[] args) {
         int arr[]={5,6,7,};
        foccurance(arr, 5, 0);
-        
     }
     public static void foccurance(int[] arr ,int key , int i) {
         if(i==arr.length-1){
