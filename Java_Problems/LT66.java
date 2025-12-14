8dd3e3051663569b23ba69bce9c69ac607eaeb8b
Adityahatake <shivanshdaksh567@gmail.com>
2025-12-14 23:54:24 +0530

    I

--- Java_Problems/LT66.java
+++ Java_Problems/LT66.java
@@ -2,6 +2,7 @@ public class LT66 {
     public static int[] plusOne(int[] digits) {
         int n = digits.length;
         
+        
         for (int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
