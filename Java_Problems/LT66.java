640c95035a3549896b958c596346e637537c6361
Adityahatake <shivanshdaksh567@gmail.com>
2025-12-31 23:18:51 +0530

    buc

--- Java_Problems/LT66.java
+++ Java_Problems/LT66.java
@@ -3,6 +3,7 @@ public class LT66 {
         int n = digits.length;
         
         
+        
         for(int i = n - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
