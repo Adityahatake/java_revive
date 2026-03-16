4026c640534de1f7a5c9f7388de8ea76c80d4d1b
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-03-16 23:59:49 +0530

    O

--- Leetcode/questions.java
+++ Leetcode/questions.java
@@ -10,6 +10,7 @@
         }
         
         // 1. Two Sum
+        
         public int[] twoSum(int[] nums, int target) {
             java.util.Map<Integer, Integer> map = new java.util.HashMap<>();
             for (int i = 0; i < nums.length; i++) {
