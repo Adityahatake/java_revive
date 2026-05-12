3a42870c0fef762cac8d3a141796a1dbc70a6465
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-05-12 23:57:34 +0530

    f g 11-05-26

--- Alpha_java/14_strings/StringImmutability.java
+++ Alpha_java/14_strings/StringImmutability.java
@@ -3,7 +3,6 @@ public class StringImmutability {
         String original = "Hello";
         String modified = original.concat(" World");
 
-        
         System.out.println("Original: " + original);  // Still "Hello"
         System.out.println("Modified: " + modified);  // New string "Hello World"
     }
