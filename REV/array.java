39599c3f96c1b42637444dfd5786c85842d89358
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-02 23:53:19 +0530

    LTK

---
+++ REV/array.java
@@ -0,0 +1,8 @@
+public class array {
+    public static void main(String[] args) {
+        // int arr[] = {1,2,3,4,5,6};
+       int arr2[]= new int[]{1, 2, 3};
+       
+        System.out.println(arr2[1]);
+    }
+}
