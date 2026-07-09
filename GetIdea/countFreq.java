2021db748e9b21c715f5185d3bf759bbd0de303b
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-10 00:10:19 +0530

    SM2.5

--- GetIdea/countFreq.java
+++ GetIdea/countFreq.java
@@ -1,13 +1,26 @@
 public class countFreq {
     public static void main(String[] args) {
         System.out.println("test");
-        int arr[]={1,2,2,3,4,3,2,1,3,4,5,6,6,5};
+        int arr[] = { 1, 2, 2, 3, 4, 3, 2, 1, 3, 4, 5, 6, 6, 5 };
+        FreqCount(arr); 
     }
 
     public static void FreqCount(int arr[]) {
-        boolean arr2[]=new boolean[arr.length] ;
-        for(int i=0;i<arr.length;i++){
-            
+        boolean visited[] = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i])
+                continue;
+
+            int count = 1;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+        System.out.println(arr[i] + " -> " + count);
+
         }
     }
 }
