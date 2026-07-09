7bfa5d4d49f4d4f4ad22b94a305ee07b6c2fd685
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-09 23:58:16 +0530

    SM2.5

--- GetIdea/countFreq.java
+++ GetIdea/countFreq.java
@@ -1,5 +1,13 @@
 public class countFreq {
     public static void main(String[] args) {
         System.out.println("test");
+        int arr[]={1,2,2,3,4,3,2,1,3,4,5,6,6,5};
+    }
+
+    public static void FreqCount(int arr[]) {
+        boolean arr2[]=new boolean[arr.length] ;
+        for(int i=0;i<arr.length;i++){
+            
+        }
     }
 }
