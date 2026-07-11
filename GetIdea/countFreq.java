150f460a98e4a08ad508d27aaa5387f32537a327
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-11 22:42:41 +0530

    LV

--- GetIdea/countFreq.java
+++ GetIdea/countFreq.java
@@ -8,7 +8,7 @@ public class countFreq {
     public static void FreqCount(int arr[]) {
         boolean visited[] = new boolean[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            if (visited[i])
+            if (visited[i] == true)
                 continue;
 
             int count = 1;
