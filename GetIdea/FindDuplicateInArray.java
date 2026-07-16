2786761b127d443739cf4dcaee6caaacdf2a24e9
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-16 23:59:44 +0530

    LT ADD

--- GetIdea/FindDuplicateInArray.java
+++ GetIdea/FindDuplicateInArray.java
@@ -26,7 +26,15 @@ public class FindDuplicateInArray {
      */
 
     public static int findDuplicate(int[] nums) {
+        
         // Write your solution here
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    return nums[i];
+                }
+            }
+        }
         return -1;
     }
 
