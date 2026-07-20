911cb7ef588643b8038ee6f644c731362d6e7ded
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-20 23:13:36 +0530

    ADD

--- Week_1/Array/twoSum.java
+++ Week_1/Array/twoSum.java
@@ -5,10 +5,11 @@ public class twoSum {
         int target=9;
        int ans[]=two_sum(arr, target);
        System.out.println(ans[0]+" "+ans[1]);
+
     }
 
     public static int[] two_sum(int nums[], int target){
-         for(int i=0;i<nums.length;i++){
+         for(int i=0;i<nums.length;i++){s
         for(int j=i+1;j<nums.length;j++){
             if(nums[i]+nums[j]==target){
                 return new int[] {i,j};
