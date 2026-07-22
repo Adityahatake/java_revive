ff7aac72e4ed1e8b00d6bae8729bf904efab7f6c
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-22 19:53:13 +0530

    opt rev

---
+++ Week_1/Array/reverse.java
@@ -0,0 +1,25 @@
+public class reverse {
+    public static void main(String[] args) {
+        System.out.println("Reverse Array(Optimised approach):");
+         int arr[] = { 1, 2, 3, 4, 5, 6 };
+         rev(arr);
+         printarray(arr);
+    }
+    public static void rev(int[] arr) {
+        int s=0;
+        int e=arr.length-1;
+        while(s<e){
+            arr[s]=arr[s]+arr[e];
+            arr[e]=arr[s]-arr[e];
+            arr[s]=arr[s]-arr[e];
+            s++;
+            e--;
+        }
+    }
+    public static void printarray(int arr[]){
+        for(int i=0;i<arr.length;i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+}
