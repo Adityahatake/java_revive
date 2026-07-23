930e49d87df9c029feefe90c23145a6e2076de2f
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-23 18:16:44 +0530

    tsa_update

--- Week_1/Array/print_subarrays.java
+++ Week_1/Array/print_subarrays.java
@@ -6,16 +6,31 @@ public class print_subarrays {
     }
     public static void subarrays(int[] arr) {
     int tsa=0;
+    int min=Integer.MAX_VALUE;
+    int max=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
+            
             for(int j=i;j<arr.length;j++){
+                int sum=0;
                 for (int k = i; k <=j; k++) {
                     System.out.print(arr[k]+" ");
+                    sum=sum+arr[k];
+                    
                 }
+                if(sum>max){
+                    max=sum;
+                }
+                if(sum<min){
+                    min=sum;
+                }
+                System.out.println(" sum:"+sum);
                 System.out.println();
                 tsa++;
             }
             System.out.println();
         }
         System.out.println("Total subarrays: "+tsa);
+        System.out.println("MAX: "+max);
+        System.out.println("MIN: "+min);
     }
 }
