7907aae428c4c813d86d26594ee384ac82e9c25f
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-23 18:06:52 +0530

    Tsa

--- Week_1/Array/print_subarrays.java
+++ Week_1/Array/print_subarrays.java
@@ -5,14 +5,17 @@ public class print_subarrays {
         subarrays(arr);
     }
     public static void subarrays(int[] arr) {
+    int tsa=0;
         for(int i=0;i<arr.length;i++){
             for(int j=i;j<arr.length;j++){
-                for(int k=i;k<j;k++){
+                for (int k = i; k <=j; k++) {
                     System.out.print(arr[k]+" ");
                 }
                 System.out.println();
+                tsa++;
             }
             System.out.println();
         }
+        System.out.println("Total subarrays: "+tsa);
     }
 }
