786229ca32cc938adb75dfffd02d35605891b1ea
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-25 23:37:33 +0530

    Bs

--- Week_1/Array/Sorting/bubbleSort.java
+++ Week_1/Array/Sorting/bubbleSort.java
@@ -1,19 +1,23 @@
-public class bubbleSort{
+public class bubbleSort {
     public static void main(String[] args) {
         System.out.println("Bubble Sort: ");
-        int arr[]={2,3,5,1,4,3,24,43,14,24,76,68,87};
+        int arr[] = { 2, 3, 5, 1, 4, 3, };
         bubble(arr);
-        for(int num:arr){
-            System.out.print(num+" ");
+        System.out.print("{");
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
+        System.out.print("}");
+
     }
+
     public static void bubble(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if (arr[i]>arr[j]) {
-                    arr[i]=arr[i]+arr[j];
-                    arr[j]=arr[i]-arr[j];
-                    arr[i]=arr[i]-arr[j];
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j =0; j < arr.length-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
             }
         }
