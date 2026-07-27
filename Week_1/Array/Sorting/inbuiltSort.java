2d18e10d15fcc6709bd68a89950b3280a762b108
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-27 23:25:56 +0530

    IBS EF

--- Week_1/Array/Sorting/inbuiltSort.java
+++ Week_1/Array/Sorting/inbuiltSort.java
@@ -1,11 +1,21 @@
 import java.util.Arrays;
+import java.util.Collections;
 public class inbuiltSort {
     public static void main(String[] args) {
-        int arr[]={3,2,4,5,1};
-        Arrays.sort(arr);
+        Integer arr[]={3,2,4,5,1};
+    //     Arrays.sort(arr);
+    // for (int i : arr) {
+    //     System.out.print(i+" ");
+    // }
+    // System.out.println();
+    // Arrays.sort(arr ,0,3);
+    Arrays.sort(arr , Collections.reverseOrder());
+
     for (int i : arr) {
         System.out.print(i+" ");
     }
-    }
+
+
+    }  
 
 }
