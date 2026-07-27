52085cd3716bca61fe5aa2cfb22b7ad547c47a19
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-07-27 23:19:45 +0530

    IBS

---
+++ Week_1/Array/Sorting/inbuiltSort.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class inbuiltSort {
+    public static void main(String[] args) {
+        int arr[]={3,2,4,5,1};
+        Arrays.sort(arr);
+    for (int i : arr) {
+        System.out.print(i+" ");
+    }
+    }
+
+}
