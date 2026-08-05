41eac4d07d07d2c619bb7ad163aa02f60a06081c
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-08-05 23:47:15 +0530

    JIK

--- Week_1/Array/Sorting/inbuiltSort.java
+++ Week_1/Array/Sorting/inbuiltSort.java
@@ -1,6 +1,8 @@
 import java.util.Arrays;
 import java.util.Collections;
 public class inbuiltSort {
+
+    
     public static void main(String[] args) {
         Integer arr[]={3,2,4,5,1};
     //     Arrays.sort(arr);
@@ -10,12 +12,9 @@ public class inbuiltSort {
     // System.out.println();
     // Arrays.sort(arr ,0,3);
     Arrays.sort(arr , Collections.reverseOrder());
-
     for (int i : arr) {
         System.out.print(i+" ");
     }
-
-
     }  
 
 }
