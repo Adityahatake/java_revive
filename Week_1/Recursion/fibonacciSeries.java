d2b3e3ac0d4dd6d9c84332e6a85b8f42118100f8
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-08-13 22:58:54 +0530

    FSS+FNN

---
+++ Week_1/Recursion/fibonacciSeries.java
@@ -0,0 +1,28 @@
+public class fibonacciSeries {
+    public static void main(String[] args) {
+        //print n fibonacci number
+       System.out.println( print(5));
+        System.out.println();
+        printseries(5);
+    }
+
+    public static int print(int n) {
+        //base case 
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+
+        //recursion
+       return  print(n-1)+print(n-2);
+    }
+
+    public static void printseries(int n) {
+        //base case
+        for(int i=0;i<=n;i++){
+            System.out.print(print(i) +" ");
+        } 
+    }
+}
