12a3a6e72a957ecdf05c2034985cb3749fd796ab
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-08-14 23:03:41 +0530

    HIT

---
+++ Week_1/Recursion/first_occurance.java
@@ -0,0 +1,20 @@
+public class first_occurance {
+    public static void main(String[] args) {
+        System.out.println("print the first occurance ");
+                int[] arr = {5, 2, 8, 2, 9};
+
+        System.out.println("First occurrence: " + check(arr, 9, 0));
+    }
+    public static int check(int[] arr , int n , int i) {
+        //base case
+        if(i==arr.length){
+            return -1;
+        }
+        //recursive case
+        if(arr[i]==n){
+            return i;
+        }
+        return check(arr, n , i=i+1);
+        
+    }
+}
