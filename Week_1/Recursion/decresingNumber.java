fa911c13d248cc9aa0b831e48d531c19580ec766
Aditya Daksh <146478804+Adityahatake@users.noreply.github.com>
2026-08-20 23:57:25 +0530

    KITH

--- Week_1/Recursion/decresingNumber.java
+++ Week_1/Recursion/decresingNumber.java
@@ -7,6 +7,8 @@ public class decresingNumber {
         //base case
         if(n==1){
             System.out.println(1);
+            
+
         }else{
                     // recursion
         System.out.print(n+" ");
@@ -15,5 +17,5 @@ public class decresingNumber {
 
 
     }
-
+  
 }
